06951dc10281523f897c009ae7fb59fe32831d79
Wilber Hasty <wilberhasty75@gmail.com>
2023-08-10 21:39:39 -0500

    Part 1

--- part1/exercises/src/components/ChoresList.js
+++ part1/exercises/src/components/ChoresList.js
@@ -1,3 +1,17 @@
+import classes from './ChoresList.module.css';
+
 export default function ChoresList () {
-   return <p>No content is here!</p>;
+   const chores = ["Mow Lawn", "Dishes", "Laundry"];
+   return   (   
+      <div>
+         <h3 className = {classes.choresHeading}>Movies I Watched This Month</h3>
+         <ol>
+            <li className={classes.choresText}>{chores[0]}</li>
+            <li className={classes.choresText}>{chores[1]}</li>
+
+
+         </ol>
+      </div>
+      );
+
 }
